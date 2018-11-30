71d047383f74a21b56f01c2f3f9d4de7ba298ac7
Ali <ali@slotherin.com>
2018-11-29 19:13:25 -0800

    more stuff

--- app.js
+++ app.js
@@ -11,3 +11,37 @@ var PORT = 7015;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+// Set up vars and data
+var reservations = [
+    {
+      name: "Yoda",
+      phone: "Jedi Master",
+      email: 900,
+      id: 2000
+    },
+    {
+      routeName: "darthmaul",
+      name: "Darth Maul",
+      role: "Sith Lord",
+      age: 200,
+      forcePoints: 1200
+    },
+    {
+      routeName: "obiwankenobi",
+      name: "Obi Wan Kenobi",
+      role: "Jedi Master",
+      age: 55,
+      forcePoints: 1350
+    }
+  ];
+
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+  
